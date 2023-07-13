969ae412c559d1e01a5463e8e7726f34eaafaec8
Eli Flores <eli@mignonnesaurus.com>
2023-07-13 22:35:29 +0200

    Use ESM modules (#5)
    * Add "type": "module" in package.json
    
    * Fix `yarn test`
    
    By migrating the jest.config.js to TypeScript and using node --experimental-vm-modules to run jest.
    Also had to install `ts-node` to fix the following error:
    ```
     Error: Jest: 'ts-node' is required for the TypeScript configuration files. Make sure it is installed
    ```
    
    * Fix `yarn lint`
    
    By migrating the .eslintrc.js to JSON.
    
    * Update tsconfig.json
    
    To set "target" and ""module" to "ESNext" and remove the "outDir" attribute.
    
    Reference: ECMA Script Modules in Node.js: https://www.typescriptlang.org/docs/handbook/esm-node.html

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[t]sx?$",
 };
+
+export default config;
